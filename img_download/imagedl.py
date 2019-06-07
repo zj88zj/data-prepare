805125749a9d84cfea830b83b006f01bb182a13d
zj88zj <jing@github>
2019-06-07 10:45:37 -0400

    csv flie

--- img_download/imagedl.py
+++ img_download/imagedl.py
@@ -3,6 +3,7 @@ import json
 import hashlib
 import argparse
 import os
+import numpy as np
 import pandas as pd
 import dask.dataframe as dd
 from dask.multiprocessing import get
@@ -19,10 +20,11 @@ parser.add_argument('--max_workers', default=10, type=int,
 
 args = parser.parse_args()
 
-# extract imagelinks
+## extract imagelinks
 df = pd.read_csv(args.products_info_json)
+df['product_hash'] = df['data.url'].apply(lambda x: hashlib.md5(x.encode('utf-8')).hexdigest())
 def parse(df):
-    return df.images.apply(lambda x: [{'image_url': eachimg['uri'], 'category': eachbox['category'], **eachbox['bounding_box']} for eachimg in json.loads(x) if 'detections' in eachimg and len(eachimg['detections']) for eachbox in eachimg['detections']])
+    return df.apply(lambda x: [{'product_hash': x['product_hash'], 'product_category': x['category'], 'image_url': eachimg['uri'], 'category': eachbox['category'], **eachbox['bounding_box']} for eachimg in json.loads(x.images) if 'detections' in eachimg and len(eachimg['detections']) for eachbox in eachimg['detections']], axis = 1)
 ddf = dd.from_pandas(df, npartitions=40)
 info = ddf.map_partitions(parse, meta=list).compute(get=get)
 info_list = [l for item in info.tolist() for l in item] 
@@ -61,6 +63,20 @@ downloader.download_images(products_df.image_url.values, args.image_fd, max_work
 
 image_hashes = list(map(lambda x: x.split('.')[0], os.listdir(args.image_fd)))
 products_df = products_df[products_df.image_hash.isin(image_hashes)].copy()
-products_df.to_csv(args.products_tgt_csv, index=False)
+products_df['attributes'] = '{}'
+products_df['source'] = 'vip'
+products_df['product0street1'] = 0
+products_df = products_df.rename(index=str, columns={'width': 'w', 'height': 'h', 'image_hash': 'orig_name'}).copy()
+df_diff, df_same = [x for _, x in products_df.groupby(products_df['category'] == products_df['product_category'])]
+df_dupprod = df_same[df_same.duplicated(subset = 'product_hash', keep = False)]
+df_roundbox = df_dupprod.round({'h': 0, 'w': 0, 'x': 0, 'y': 0}).astype({'h': int, 'w': int, 'x': int, 'y': int}).reset_index(drop=True)
+nums = np.random.choice(range(df_roundbox['product_hash'].nunique()), size = df_roundbox['product_hash'].nunique(), replace=False) 
+df_roundbox['product_idx'] = df_roundbox['product_hash'].map(dict(zip(df_roundbox['product_hash'].unique(), nums)))
+df_roundbox.to_csv(args.products_tgt_csv, index=False)
 
-
+## filter images
+hash_list = df_roundbox['orig_name'].to_list()
+sub = list(set(image_hashes)-set(hash_list))
+for name in sub:
+   filename = '{}.jpg'.format(name)
+   os.remove(os.path.join('./image/', filename))
